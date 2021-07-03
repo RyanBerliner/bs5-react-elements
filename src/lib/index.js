d115333ab33ed4fb8a02e98f13dc9e9fa60c082c
Ryan Berliner <22206986+RyanBerliner@users.noreply.github.com>
2021-07-02 21:55:43 -0400

    add toast element (#18)
    * adds toast element and tests
    
    * add example usage

--- src/lib/index.js
+++ src/lib/index.js
@@ -2,4 +2,5 @@ export {Alert} from './Alert.jsx';
 export {Modal} from './Modal.jsx';
 export {Offcanvas} from './Offcanvas.jsx';
 export {Popover} from './Popover.jsx';
+export {Toast} from './Toast.jsx';
 export {Tooltip} from './Tooltip.jsx';
