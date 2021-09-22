49e9d30d63646963601ae3d0d2816f9501a91255
Ryan Berliner <ryan.berliner@gmail.com>
2021-09-22 19:36:49 -0400

    poc for rendering title and content of popover

--- src/lib/Popover.jsx
+++ src/lib/Popover.jsx
@@ -1,4 +1,4 @@
-import React, {useRef, useMemo} from 'react';
+import React, {useRef, useMemo, useCallback, useState} from 'react';
 import PropTypes from 'prop-types';
 import {Popover} from 'bootstrap';
 import {useBootstrap} from './hooks.js';
@@ -16,25 +16,80 @@ function PopoverComponent({
   config,
   children,
   as: ElementType,
+  renderTitle,
+  renderContent,
   ...props
 }) {
   const componentElement = useRef();
+  // popover-header
+  // popover-body
+  const [tip, setTip] = useState();
+
+  const wrappedOnInserted = useCallback((event) => {
+    const tip = Popover.getInstance(componentElement.current).getTipElement();
+
+    if (renderTitle) {
+      tip.querySelector('.popover-header').innerHTML = '';
+    }
+
+    if (renderContent) {
+      tip.querySelector('.popover-body').innerHTML = '';
+    }
+
+    setTip(tip);
+
+    if (onInserted) {
+      onInserted(event);
+    }
+  }, [onInserted, renderTitle, renderContent]);
+
+  const wrappedOnHide = useCallback((event) => {
+    setTip(null);
+
+    if (onHide) {
+      onHide(event);
+    }
+  }, [onHide]);
 
   const events = useMemo(() => {
     return new Map([
       ['shown.bs.popover', onShown],
       ['show.bs.popover', onShow],
       ['hidden.bs.popover', onHidden],
-      ['hide.bs.popover', onHide],
-      ['inserted.bs.popover', onInserted],
+      ['hide.bs.popover', wrappedOnHide],
+      ['inserted.bs.popover', wrappedOnInserted],
     ]);
-  }, [onShown, onShow, onHidden, onHide, onInserted]);
+  }, [onShown, onShow, onHidden, wrappedOnHide, wrappedOnInserted]);
+
+  if (!config) {
+    config = {};
+  }
+
+  if ((renderTitle || renderContent) && config.animation !== false) {
+    config.animation = false;
+  }
 
   useBootstrap(Popover, config, component, componentElement, events);
+
+  if (renderTitle) {
+    props.title = ' ';
+  }
+
+  if (renderContent) {
+    props['data-bs-content'] = ' ';
+  }
 
   return (
     <ElementType ref={componentElement} {...props}>
       {children}
+      {(tip && renderTitle) && ReactDOM.createPortal(
+          renderTitle(Popover.getInstance(componentElement.current)),
+          tip.querySelector('.popover-header'),
+      )}
+      {(tip && renderContent) && ReactDOM.createPortal(
+          renderContent(Popover.getInstance(componentElement.current)),
+          tip.querySelector('.popover-body'),
+      )}
     </ElementType>
   );
 }
@@ -79,6 +134,14 @@ PopoverComponent.propTypes = {
    * The element type of the popover
    */
   as: PropTypes.elementType,
+  /**
+   * Render popover title as a react component
+   */
+  renderTitle: PropTypes.func,
+  /**
+   * Render popover content as a react component
+   */
+  renderContent: PropTypes.func,
 };
 
 PopoverComponent.defaultProps = {
