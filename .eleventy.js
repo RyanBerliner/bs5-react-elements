2780e8d19c930d5965b80433146c5effde278744
Ryan Berliner <ryan.berliner@gmail.com>
2021-12-05 09:41:22 -0500

    remove hardcoded package version in install instructions

--- .eleventy.js
+++ .eleventy.js
@@ -1,3 +1,5 @@
+const packageJson = require('./package.json');
+
 module.exports = function(eleventyConfig) { 
   eleventyConfig.addFilter("exampleusage", function(value) {
     return `exampleusage/${value.toLowerCase()}.njk`;
@@ -10,6 +12,13 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addFilter("propsince", function(value) {
     return value.split('@since ')[1];
   });
+
+  // usage: {{ 'minor' | packageversion }}
+  // output: 2.1
+  eleventyConfig.addFilter("packageversion", function(value) {
+    const type = ['major', 'minor', 'patch'];
+    return packageJson.version.split('.').slice(0, type.indexOf(value) + 1).join('.');
+  })
 
   eleventyConfig.addPassthroughCopy({'dist': 'assets/dist'});
 
