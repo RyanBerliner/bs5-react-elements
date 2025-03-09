c1c081ce531cbdb974aa645df1bfe31eb5920f88
Ryan Berliner <ryan.berliner@gmail.com>
2025-03-09 17:21:01 -0400

    wip

--- setupJest.js
+++ setupJest.js
@@ -10,3 +10,28 @@ jest.mock('bootstrap', () => {
 
   return jest.requireActual(bootstrapVersions[version]);
 });
+
+
+jest.mock('react', () => {
+  const reactVersion = process.env.REACT_VERSION || 'latest';
+
+  const reactVersions = {
+    '17': 'react17',
+    '18': 'react18',
+    'latest': 'react',
+  };
+
+  return jest.requireActual(reactVersions[reactVersion]);
+});
+
+jest.mock('react-dom', () => {
+  const reactVersion = process.env.REACT_VERSION || 'latest';
+
+  const reactDOMVersions = {
+    '17': 'react-dom17',
+    '18': 'react-dom18',
+    'latest': 'react-dom',
+  };
+
+  return jest.requireActual(reactDOMVersions[reactVersion]);
+});
